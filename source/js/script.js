9bdabd9e64a67c4ceae05be6dd172570481338eb
Sergery Rutkousky <dr.bibelman@gmail.com>
2022-01-13 13:13:11 +0300

    Ошибка Б31. Добавляет в карточке товара ссылки к изображени и заголовку товара. Добавляет стилизацию фокуса для них.

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ noJsMap.classList.remove("contacts__map--no-js");
 const noJsClass = document.querySelector(".navbar");
 noJsClass.classList.remove("navbar--no-js");
 const mainNav = document.querySelector('.main-nav');
-const toogleButton = document.querySelector('.navbar__toggle');
+const toogleButton = document.querySelector(".navbar__toggle");
 toogleButton.onclick = function () {
   mainNav.classList.toggle('main-nav--visible');
   toogleButton.classList.toggle('navbar__toggle--active');
